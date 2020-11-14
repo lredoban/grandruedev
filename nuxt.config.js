c1f2043c458db8c487965586148e6a2ec3832557
Lova <maitrejedi@lovasoa.fr>
2020-11-14 12:32:57 +0100

    chore: add PWA support and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,8 +49,20 @@ export default {
           Caveat: true
         }
       }
-    ]
+    ],
+    // https://pwa.nuxtjs.org
+    '@nuxtjs/pwa'
   ],
+  pwa: {
+    manifest: {
+      name: "Grand'Rue",
+      short_name: "Grand'Rue",
+      description:
+        'Grand’Rue a l’ambition de simplifier et d’augmenter le commerce local en connectant tous les acteurs d’un territoire au moyen d’une porte d’entrée unique',
+      background_color: '#5F8B7A',
+      lang: 'fr'
+    }
+  },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
