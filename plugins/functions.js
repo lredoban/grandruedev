5fbde4d537459d5e0c1a0066179cf6fc59a5dde5
Lova <maitrejedi@lovasoa.fr>
2020-11-19 12:47:21 +0100

    chore test deploy to netlify

--- plugins/functions.js
+++ plugins/functions.js
@@ -2,9 +2,7 @@ export default function ({ $http, env }, inject) {
   const $functions = $http.create({
     // See https://github.com/sindresorhus/ky#options
   })
-  $functions.setBaseURL(
-    process.env.DEPLOY_URL || 'http://localhost:8888/.netlify/functions/'
-  )
+  $functions.setBaseURL('http://localhost:8888/.netlify/functions/')
 
   // Inject to context as $functions
   inject('functions', $functions)
