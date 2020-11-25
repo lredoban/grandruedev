de9ef92d3ff4b783bfb0dca7c4eab9b97f507795
Lova <maitrejedi@lovasoa.fr>
2020-11-25 13:31:50 +0100

    go

--- api/productsBy.js
+++ api/productsBy.js
@@ -6,15 +6,18 @@ exports.productsBy = ({ key, param }) => {
     .select({ filterByFormula: `${key}="${param}"` })
     .firstPage()
     .then((records) => {
-      return records.map((record) => ({
-        slug: record.fields.ref.toLowerCase(),
-        name: record.fields.name,
-        price: record.fields.price,
-        unit: record.fields.unit,
-        storeAddress: `${record.fields.storeCity[0]} (${record.fields.storeZipCode[0]})`,
-        storeName: record.fields.storeName[0],
-        imgUrl: record.fields.images[0].url
-      }))
+      return records.map((record) => {
+        return {
+          slug: record.fields.ref.toLowerCase(),
+          name: record.fields.name,
+          price: record.fields.price,
+          unit: record.fields.unit,
+          storeAddress: `${record.fields.storeCity[0]} (${record.fields.storeZipCode[0]})`,
+          storeName: record.fields.storeName[0],
+          imgUrl: record.fields.images[0].url,
+          subCategories: record.fields.subNames
+        }
+      })
     })
     .catch((err) => {
       logger.error(err)
--- pages/categories/_slug.vue
+++ pages/categories/_slug.vue
@@ -1,8 +1,10 @@
 <script>
 import ClickOutside from 'vue-click-outside'
+import { CollapseTransition } from 'vue2-transitions'
 
 export default {
   name: 'CategoryPage',
+  components: { CollapseTransition },
   directives: { ClickOutside },
   async asyncData({ $db, params }) {
     const category = await $db.fetch('categoryBySlug', { slug: params.slug })
@@ -21,8 +23,24 @@ export default {
     }
   },
   data: () => ({
-    displayCategories: false
-  })
+    displayCategories: false,
+    displaySubs: false,
+    subFilter: ''
+  }),
+  computed: {
+    filteredProducts() {
+      if (!this.subFilter) return this.products
+      return this.products.filter((p) =>
+        p.subCategories.includes(this.subFilter)
+      )
+    }
+  },
+  methods: {
+    selectFilter(name) {
+      this.subFilter = name
+      this.displaySubs = false
+    }
+  }
 }
 </script>
 
@@ -42,41 +60,74 @@ export default {
     <div class="px-4 bg-red-600 flex justify-between">
       <div class="relative">
         <button
-          class="p-2 uppercase font-bold tracking-wide text-white text-opacity-75"
+          class="p-2 uppercase font-bold tracking-wide text-white text-opacity-75 flex items-center gap-x-2"
           @click.stop="displayCategories = !displayCategories"
         >
           {{ category.name }}
+          <div :class="displayCategories && 'transform rotate-180'">
+            <IArrowDown class="h-3" />
+          </div>
         </button>
-        <nav
-          v-show="displayCategories"
-          v-click-outside="() => (displayCategories = false)"
-          class="absolute top-0 -ml-4 px-4 mt-10 max-w-xs w-screen z-10 bg-black bg-opacity-75"
-        >
-          <n-link
-            v-for="cat in categories"
-            :key="cat.slug"
-            tag="div"
-            :to="{ name: 'categories-slug', params: { slug: cat.slug } }"
-            class="block p-2 uppercase font-bold tracking-wide text-white text-opacity-75"
-            >{{ cat.name }}</n-link
+        <CollapseTransition>
+          <nav
+            v-show="displayCategories"
+            v-click-outside="() => (displayCategories = false)"
+            class="absolute top-0 -ml-4 px-4 mt-10 max-w-xs w-screen z-10 bg-black bg-opacity-75"
           >
-        </nav>
+            <n-link
+              v-for="cat in categories"
+              :key="cat.slug"
+              :to="{ name: 'categories-slug', params: { slug: cat.slug } }"
+              class="block p-2 uppercase font-bold tracking-wide text-white text-opacity-75"
+              >{{ cat.name }}</n-link
+            >
+          </nav>
+        </CollapseTransition>
       </div>
-      <button
-        class="p-2 uppercase font-bold tracking-wide text-white text-opacity-75"
-      >
-        Filtrer
-      </button>
+      <div class="relative">
+        <button
+          class="p-2 uppercase font-bold tracking-wide text-white text-opacity-75"
+          @click.stop="displaySubs = !displaySubs"
+        >
+          {{ subFilter || 'Filtrer' }}
+        </button>
+        <CollapseTransition>
+          <ul
+            v-show="displaySubs"
+            v-click-outside="() => (displaySubs = false)"
+            class="absolute top-0 right-0 -mr-4 px-4 mt-10 max-w-xs w-screen z-10 bg-black bg-opacity-75 text-right"
+          >
+            <li>
+              <button
+                class="block ml-auto p-2 uppercase font-bold tracking-wide text-white text-opacity-75 text-right"
+                @click.stop="selectFilter('')"
+              >
+                Tous les articles
+              </button>
+            </li>
+            <li v-for="{ name } in subs" :key="name">
+              <button
+                class="block ml-auto p-2 uppercase font-bold tracking-wide text-white text-opacity-75 text-right"
+                @click.stop="selectFilter(name)"
+              >
+                {{ name }}
+              </button>
+            </li>
+          </ul>
+        </CollapseTransition>
+      </div>
     </div>
     <div class="mt-4 text-center text-sm text-gray-500 text-hairline italic">
       {{ products.length }} Articles trouvés
     </div>
     <section class="mt-8 px-4 grid grid-cols-2 gap-x-4 gap-y-6">
-      <ProductExerpt
-        v-for="product in products"
+      <n-link
+        v-for="product in filteredProducts"
         :key="product.slug"
-        :product="product"
-      />
+        :to="{ name: 'products-slug', params: { slug: product.slug } }"
+      >
+        <ProductExerpt :product="product" />
+      </n-link>
     </section>
   </main>
 </template>
